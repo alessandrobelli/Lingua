bd04005827889436cc73662f04c1a906ebdda6b5
Alessandro Belli <alessandrobelli90@gmail.com>
2020-10-16 12:38:41 +0200

    Update CsvImport.php
    * improved csv import.

--- src/Http/Livewire/CsvImport.php
+++ src/Http/Livewire/CsvImport.php
@@ -41,46 +41,27 @@ class CsvImport extends Component
         $path = $this->csv->getRealPath();
         $data = array_map('str_getcsv', file($path));
         $this->csv_data = $data;
+        $this->emit('show-toast', count($data)." long", 'error');
+
         for ($i = 1; $i < count($data); $i++) {
             $string = str_replace('"', "", $data[$i][($this->stringToBeTranslatedField - 1)]);
             $language = $data[$i][($this->languageField - 1)];
             $translation = $data[$i][($this->translatedStringField - 1)];
             $project = $this->projectLabelString ? $data[$i][($this->projectLabelString - 1)] : '';
-            $existingTranslation = Translation::where('string', $string)->first();
-            if (! $existingTranslation) {
+
+            $existingString = Translation::where('string', $string)->first();
+            $translationLanguage = LinguaUtilities::array_multidimensional_search(config('lingua.locales-list'), 'isolanguagename', $language)[0]['locale'];
+            $this->addLocaleIfNotExist($translationLanguage);
+            if (!$existingString)
+            {
                 // CHECK WHY MULTI WORDS ARE CONSIDERED NOT THE SAME
-                $newTranslation = Translation::create(['string' => $string, 'project' => $project]);
-                $json_array = $newTranslation->locales;
-                $json_array[LinguaUtilities::array_multidimensional_search(config('lingua.locales-list'), 'isolanguagename', $language)[0]['locale']] = $translation;
-                $newTranslation->locales = $json_array;
-                $newTranslation->save();
-                foreach (Translation::AllLocales() as $locale) {
-                    if ($locale != LinguaUtilities::array_multidimensional_search(config('lingua.locales-list'), 'isolanguagename', $language)[0]['locale']) {
-                        $json_array = $newTranslation->locales;
-                        if (array_key_exists($locale, $json_array)) {
-                            $json_array[$locale] = ! is_null($json_array[$locale]) ? $json_array[$locale] : "";
-                        } else {
-                            $json_array[$locale] = "";
-                        }
-                        $newTranslation->locales = $json_array;
-                        $newTranslation->save();
-                    }
-                }
-            } else {
-                $languageToAdd = LinguaUtilities::array_multidimensional_search(config('lingua.locales-list'), 'isolanguagename', $language)[0]['locale'];
-                foreach (Translation::all() as $translation) {
-                    $json_array = $translation->locales;
-                    $json_array[$languageToAdd] = "";
-                    $translation->locales = $json_array;
-                    $translation->save();
-                }
-                $json_array = $existingTranslation->locales;
-                $json_array[$languageToAdd] = $translation;
-                $existingTranslation->locales = $json_array;
-                $existingTranslation->project = $project;
-                $existingTranslation->save();
-                //update the value you get back
+                $existingString = Translation::create(['string' => $string, 'project' => $project]);
             }
+            $json_array = $existingString->locales;
+            $json_array[$translationLanguage] = $translation;
+            $existingString->locales = $json_array;
+            $existingString->project = $project;
+            $existingString->save();
         }
         $this->emit('show-toast', 'Successfully imported CSV', 'success');
     }
@@ -88,5 +69,24 @@ class CsvImport extends Component
     public function render()
     {
         return view('lingua::livewire.csv-import');
+    }
+
+    /**
+     * @param $translationLanguage
+     */
+    private function addLocaleIfNotExist($translationLanguage)
+    {
+        $translationAndLocale = Translation::whereNotNull('locales->' . $translationLanguage)->first();
+        if (!$translationAndLocale)
+        {
+            //  locale doesn't. add it to the whole translations.
+            foreach (Translation::all() as $translation)
+            {
+                $json_array = $translation->locales;
+                $json_array[$translationLanguage] = "";
+                $translation->locales = $json_array;
+                $translation->save();
+            }
+        }
     }
 }
