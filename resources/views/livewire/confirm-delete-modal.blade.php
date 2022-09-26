1f6ab9dc71c7fa8d8933bbc7a45287725345e8e0
Alessandro Belli <belli@uni-bremen.de>
2022-09-26 10:39:31 +0200

    Compatibility with php 8.1 and laravel 9

--- resources/views/livewire/confirm-delete-modal.blade.php
+++ resources/views/livewire/confirm-delete-modal.blade.php
@@ -56,10 +56,10 @@
                     <div class="flex justify-end pt-2">
                         <button class="modal-close px-4 bg-indigo-600 p-3 rounded-lg text-white hover:bg-indigo-400 mr-2" wire:click="$emit('closeModal')">Cancel</button>
                         @if($entityToDelete === "merge translations")
-                            <button class="modal-close px-4 bg-red-600 p-3 rounded-lg text-white hover:bg-red-400 " wire:click="delete()">Merge</button>
+                            <button class="inline-flex items-center justify-center p-2 text-gray-400 rounded-md hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" wire:click="delete()">Merge</button>
 
                         @else
-                            <button class="modal-close px-4 bg-red-600 p-3 rounded-lg text-white hover:bg-red-400 " wire:click="delete()">Delete</button>
+                            <button class="inline-flex items-center justify-center p-2 text-red-400 rounded-md hover:text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" wire:click="delete()">Delete</button>
 
                         @endif
 
